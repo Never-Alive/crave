36e49c9cf4fc460b1eeb125f2995fd02c38c65e3
Never-Alive <livinglucifer0@gmail.com>
2026-02-14 14:10:13 +0530

    poop

---
+++ build_script.sh
@@ -0,0 +1,56 @@
+# Rom repo init
+
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+
+echo "-----------------------------"
+echo "Repo init cloned successfully"
+echo "-----------------------------"
+
+# sycning
+
+echo "-----------------------"
+echo "Starting to sync source"
+echo "-----------------------"
+
+/opt/crave/resync.sh
+
+/opt/crave/resync.sh
+
+/opt/crave/resync.sh
+
+echo "------------------------"
+echo "Source syncing comppeted"
+echo "------------------------"
+
+
+# Dt
+
+git clone https://github.com/Never-Alive/device_xiaomi_haydn -b lineage-23.2 device/xiaomi/haydn
+
+echo "---------------------"
+echo "Tress clone completed"
+echo "---------------------"
+
+# build env
+
+. b*/e*
+
+echo "---------------------------"
+echo "Build/envsetup.sh completed"
+echo "---------------------------"
+
+# signing script
+
+curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh | bash
+
+echo "----------------------------"
+echo "Rom signed with private keys"
+echo "----------------------------"
+
+# lunch
+
+brunch haydn
+
+echo "----------"
+echo "Brunch done"
+echo "----------"
